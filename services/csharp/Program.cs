ed3d32e443cb5c0821e1f39154dc2b36c3064d84
Marvin Gajek <marvin.gajek@cern.ch>
2025-11-30 18:10:52 +0100

    fix C# setup

--- services/csharp/Program.cs
+++ services/csharp/Program.cs
@@ -7,26 +7,37 @@ using OpenTelemetry.Logs;
 var builder = WebApplication.CreateBuilder(args);
 
 var serviceName = Environment.GetEnvironmentVariable("OTEL_SERVICE_NAME") ?? "csharp-service";
-var otlpEndpoint = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT") ?? "http://localhost:4317";
+var otlpEndpoint = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT") ?? "localhost:4317";
 
-// Configure OpenTelemetry
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService(serviceName))
     .WithTracing(tracing => tracing
         .AddAspNetCoreInstrumentation()
         .AddHttpClientInstrumentation()
-        .AddOtlpExporter(options => options.Endpoint = new Uri(otlpEndpoint)))
+        .AddOtlpExporter(options => 
+        {
+            options.Endpoint = new Uri($"http://{otlpEndpoint}");
+            options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+        }))
     .WithMetrics(metrics => metrics
         .AddAspNetCoreInstrumentation()
         .AddHttpClientInstrumentation()
-        .AddOtlpExporter(options => options.Endpoint = new Uri(otlpEndpoint)));
+        .AddOtlpExporter(options => 
+        {
+            options.Endpoint = new Uri($"http://{otlpEndpoint}");
+            options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+        }));
 
 // Configure logging
 builder.Logging.AddOpenTelemetry(logging =>
 {
     logging.IncludeFormattedMessage = true;
     logging.IncludeScopes = true;
-    logging.AddOtlpExporter(options => options.Endpoint = new Uri(otlpEndpoint));
+    logging.AddOtlpExporter(options => 
+    {
+        options.Endpoint = new Uri($"http://{otlpEndpoint}");
+        options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+    });
 });
 
 var app = builder.Build();
